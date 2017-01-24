79fe61b61f542cac9e00d1b3824d434e423973bf
Brian Turnbo <z1796056@hopper.cs.niu.edu>
2017-01-24 15:26:07 -0600

    Added assignment00.cc

---
+++ assignment00.cc
@@ -0,0 +1,19 @@
+/* Brian Turnbo Jr.
+   Z1796056
+   CSCI340-2
+   I certify that this is my own work and where appropriate an extension
+   of the starter code provided for the assignment.
+*/
+#include <iostream>
+using namespace std;
+
+int
+main(int argc, char *argv[])
+{
+
+  // Insert new code line replacing this comment.
+  cout << "I'm very excited to be taking Data Structures" << endl;
+
+  return 0;
+
+}// End of main() routine.
